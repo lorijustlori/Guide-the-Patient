47ab83a555e2afd4de28e5e74c93bc65e01b6f00
Lori Escobar <lorimcescobar@hotmail.com>
2018-06-13 17:33:30 -0400

    patient validations added

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+
+	validates :medical_record_number, :email,   presence: true,
+												uniqueness: true
+												:null => false
+				:first_name, :last_name, :address, :city, :state, :phone,
+					:null => false									
 end
