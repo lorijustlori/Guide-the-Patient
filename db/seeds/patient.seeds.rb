d7dea0dca8c027bcf9ee364cefa42bc30d66f48a
Greg <sportzballa2000@gmail.com>
2018-06-14 14:34:14 -0400

    add seeds and migration

---
+++ db/seeds/patient.seeds.rb
@@ -0,0 +1,13 @@
+100.times do
+  Patient.create(
+    medical_record_number:  Faker::IDNumber.valid,
+    first_name:             Faker::Name.first_name,
+    last_name:              Faker::Name.last_name,
+    address:                Faker::Address.street_address,
+    city:                   Faker::Address.city,
+    state:                  Faker::Address.state_abbr,
+    phone:                  Faker::PhoneNumber.phone_number,
+    email:                  Faker::Internet.email
+  )
+  puts "#{Patient.count} patients"
+end
