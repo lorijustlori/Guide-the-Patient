1a42897e0723fad29d1a0ae6d00297b1f31126ce
Lori Escobar <lorimcescobar@hotmail.com>
2018-06-14 18:20:55 -0400

    added action seed file, seeds.rb empty - as it should be.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Action.create(activity: "Pay Bills", number: 3, unit: "days", timeline: "before", remedy_id: 23)
-Action.create(activity:"call pet sitter", number:1, unit:"week", timeline:"before", remedy_id: 23)
-
-# Find the Remedy for these actions
-# r = Remedy.find_by(code: "<CODE HERE>")
-# r.actions.create(activity: "Pay Bills", number: 3, unit: "days", timeline: "before")
