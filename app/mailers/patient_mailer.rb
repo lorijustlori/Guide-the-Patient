156ac6299c2e3d3c59d42147840213ebe5b53958
Lori Escobar <lorimcescobar@hotmail.com>
2018-06-20 17:08:49 -0400

    updates to action seeds some CSS for main views, tabs added, images added

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,7 +1,11 @@
 class PatientMailer < ApplicationMailer
+
+	require 'securerandom'
+	
   def schedule_email
     @patient 	= params[:patient]
     @procedure 	= params[:procedure]
     mail(to: @patient.email, subject: 'Your Surgery Schedule')
-  end
+
+   end
 end
--- db/seeds/remedy.seeds.rb
+++ db/seeds/remedy.seeds.rb
@@ -9,3 +9,4 @@ CSV.foreach(
   Remedy.find_or_create_by(row.to_h)
   puts "There are now #{Remedy.count} rows in the remedies table"
 end
+ 
